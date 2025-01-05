d9f7e7b7078a5edc3ed4ad0ef6576227bbad0599
Kalash2004 <kalashsrivastava0189@gmai.com>
2025-01-05 19:58:53 +0530

    Initial commit for portfolio

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#element', {
+  strings: ['Artificial Intelligence' ,'Machine Learning','and want to dive  deep in this new world'],
+  typeSpeed: 50,
+});
+function downloadResume() {
+ 
+  const resumeUrl = "CV_2024122118324548.pdf";
+  window.open(resumeUrl, "_blank");
+}
+function visitGitHub() {
+  
+  const githubUrl = "https://github.com/Kalash2004";
+  window.open(githubUrl, "_blank");
+}
